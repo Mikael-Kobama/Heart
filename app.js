d59f893ddd40f3dac990ececf263328581b3d2c5
DevKobama <125419153+Mikael-Kobama@users.noreply.github.com>
2024-10-19 22:32:08 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+grayHeart = document.querySelector('.gray-heart');
+redHeart = document.querySelector('.red-heart');
+
+grayHeart.addEventListener('click', () => {
+    redHeart.classList.add('animation');
+    grayHeart.classList.add('fill-color');
+})
+
+redHeart.addEventListener('click', () => {
+    redHeart.classList.remove('animation');
+    grayHeart.classList.remove('fill-color');
+})
+
+console.log(redHeart);
+console.log(grayHeart);
